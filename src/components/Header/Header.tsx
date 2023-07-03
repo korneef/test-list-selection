19ec86b720c7b92fa6c6b3a02b9278bde4ed4e39
Valentin Korneev <v.korneef@gmail.com>
2023-07-03 22:06:32 +0600

    Header was ready

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,11 +5,17 @@ import './Header.css'
 function Header() {
 
   const className = 'header'
+
+  const navLinkClass = (obj: { isActive: boolean, isPending: boolean }): string => {
+    const {isActive} = obj;
+    return isActive ? 'nav-link nav-link_active' : 'nav-link';
+  }
+
   return (
-    <div className={className}>
-      <ul>
-        <li><NavLink to={'/'}>Главная</NavLink></li>
-        <li><NavLink to={'/history'}>История</NavLink></li>
+    <div className={ className }>
+      <ul className={ `${ className }__nav-bar` }>
+        <li className={ `${ className }__nav-item` }><NavLink className={ navLinkClass } to={ '/' }>Главная</NavLink></li>
+        <li className={ `${ className }__nav-item` }><NavLink className={ navLinkClass } to={ '/history' }>История</NavLink></li>
       </ul>
     </div>
   );
